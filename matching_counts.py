5b9e446871183057fcf00154cc72f2a7ebc42c87
Your Name <trachellehaddox2007@gmail.com>
2025-11-07 15:07:29 -0500

    Completed ToDo

--- matching_counts.py
+++ matching_counts.py
@@ -12,10 +12,12 @@ def count_matching(matrix, target):
     @return [int]: the number of matching elements
     """
 
-    # TODO: set initial variable `count`
-
-    # TODO: use external loop to get all nested list
-    # TODO: use inner loop to get every element of the inner list, if the element is equal to the target argument, increase `count` by 1
+    count = 0 
+    
+    for row in matrix:
+        for elem in row:
+            if elem == target:
+                count += 1
 
     return count
 
--- sudoku_1_row.py
+++ sudoku_1_row.py
@@ -14,15 +14,16 @@ def row_check(sudoku, row_no):
     @return [bool], returns True if the row is correct, False otherwise.
     """
 
-    # TODO: use set() to create a new set to record checked position. Since set doesn't allow unique items, if the set contains the same number, we know that there is a conflict
-
+    seen = set()
     row_to_check = sudoku[row_no]  # get the row to check
 
     for num in row_to_check:
-        # TODO: use loop to check every item of the row. If the number is not 0, then we check if the number is already in the set. If the number in the set, it means this number exists, directly return False; otherwise add this number to the set, and continue until all numbers are checked.
+        if num != 0:
+            if num in seen:
+                return False
+            seen.add(num)
 
-    # TODO: return True if the loop terminates normally (all numbers are checked without duplication found)
-
+    return True
 
 if __name__ == "__main__":
     sudoku = [
